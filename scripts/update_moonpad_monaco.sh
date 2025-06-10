eb01dd90bcd4178bc0da3841daf45c345324e30c
A23187 <a23187.1979421048@gmail.com>
2025-06-10 12:25:14 +0800

    update scripts/update_moonpad_monaco.sh

--- scripts/update_moonpad_monaco.sh
+++ scripts/update_moonpad_monaco.sh
@@ -5,6 +5,6 @@ cp node_modules/@moonbit/moonpad-monaco/dist/{lsp-server.js,moonc-worker.js,moon
 git add moonpad-monaco/{lsp-server.js,moonc-worker.js,moonpad-monaco.d.ts,moonpad-monaco.js,onig.wasm} package.json package-lock.json
 git commit -m 'update moonpad-monaco to latest version'
 echo -e '\e[31m\nplease manually patch moonpad-monaco.js according to the following commit\n\e[00m'
-git show 7f24cba
+git show 8766736
 # git add moonpad-monaco/moonpad-monaco.js
 # git cm -m 'patch moonpad-monaco.js'
