aa215d38c7906cd854c38a5fe66e2381bbdd88d4
A23187 <a23187.1979421048@gmail.com>
2025-06-12 17:27:57 +0800

    add github action to update moonpad-monaco

--- scripts/update_moonpad_monaco.sh
+++ scripts/update_moonpad_monaco.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 
+echo "check if @moonbit/moonpad-monaco is outdated"
+outdated=$(npm outdated --json @moonbit/moonpad-monaco | jq '.["@moonbit/moonpad-monaco"] | (.current != .latest)')
+if [ "$outdated" != "true" ]; then
+    echo "@moonbit/moonpad-monaco is up to date"
+    exit
+fi
+echo "update @moonbit/moonpad-monaco to latest version"
 npm i @moonbit/moonpad-monaco@latest
 cp node_modules/@moonbit/moonpad-monaco/dist/{lsp-server.js,moonc-worker.js,moonpad-monaco.d.ts,moonpad-monaco.js,onig.wasm} moonpad-monaco/
 git add moonpad-monaco/{lsp-server.js,moonc-worker.js,moonpad-monaco.d.ts,moonpad-monaco.js,onig.wasm} package.json package-lock.json
 git commit -m 'update moonpad-monaco to latest version'
-echo -e '\e[31m\nplease manually patch moonpad-monaco.js according to the following commit\n\e[00m'
+echo "patch moonpad-monaco.js"
 sed -i '' 's/^import \* as \([a-zA-Z_][a-zA-Z0-9_]*\) from "monaco-editor-core";$/const \1 = globalThis.monaco;/' moonpad-monaco/moonpad-monaco.js
 git add moonpad-monaco/moonpad-monaco.js
 git cm -m 'patch moonpad-monaco.js'
+echo "git push to remote repository"
+git push origin main
