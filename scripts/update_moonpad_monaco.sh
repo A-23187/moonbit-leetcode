ad4ec7f126e85343f559ca125d47df59f700f088
A23187 <a23187.1979421048@gmail.com>
2025-06-19 13:49:37 +0800

    fix scripts/update_moonpad_monaco.sh

--- scripts/update_moonpad_monaco.sh
+++ scripts/update_moonpad_monaco.sh
@@ -11,11 +11,13 @@ fi
 echo "update @moonbit/moonpad-monaco to latest version"
 npm i @moonbit/moonpad-monaco@latest
 cp node_modules/@moonbit/moonpad-monaco/dist/{lsp-server.js,moonc-worker.js,moonpad-monaco.d.ts,moonpad-monaco.js,onig.wasm} moonpad-monaco/
+git config --local user.email A23187
+git config --local user.name a23187.1979421048@gmail.com
 git add moonpad-monaco/{lsp-server.js,moonc-worker.js,moonpad-monaco.d.ts,moonpad-monaco.js,onig.wasm} package.json package-lock.json
 git commit -m 'update moonpad-monaco to latest version'
 echo "patch moonpad-monaco.js"
 sed -i '' 's/^import \* as \([a-zA-Z_][a-zA-Z0-9_]*\) from "monaco-editor-core";$/const \1 = globalThis.monaco;/' moonpad-monaco/moonpad-monaco.js
 git add moonpad-monaco/moonpad-monaco.js
-git cm -m 'patch moonpad-monaco.js'
+git commit -m 'patch moonpad-monaco.js'
 echo "git push to remote repository"
 git push origin main
