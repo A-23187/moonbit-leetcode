0df957ca34342cda068ab0d29327ebfc62f359d1
A23187 <a23187.1979421048@gmail.com>
2026-03-03 03:12:25 +0000

    patch moonpad-monaco.js

--- moonpad-monaco/moonpad-monaco.js
+++ moonpad-monaco/moonpad-monaco.js
@@ -1,7 +1,7 @@
 var Xd = Object.defineProperty;
 var Qd = (I, g, A) => g in I ? Xd(I, g, { enumerable: !0, configurable: !0, writable: !0, value: A }) : I[g] = A;
 var Dg = (I, g, A) => Qd(I, typeof g != "symbol" ? g + "" : g, A);
-import * as MI from "monaco-editor-core";
+const MI = globalThis.monaco;
 const IG = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   get BigIntStatsFs() {
